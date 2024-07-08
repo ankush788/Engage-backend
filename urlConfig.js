3ea9105fc7e7f1003d338a486a815d934f8a063c
ANKUSH SINGH <ankushsingh_se21a13_54@dtu.ac.in>
2024-07-08 13:21:21 +0530

    Update urlConfig.js

--- urlConfig.js
+++ urlConfig.js
@@ -1,5 +1,5 @@
 const FRONTEND_URL =
   process.env.NODE_ENV === "development"
-    ? `http://localhost:3000`
+    ? `https://engage-frontend-chi.vercel.app`
     : `https://engage-frontend-chi.vercel.app`;
 module.exports = FRONTEND_URL;
